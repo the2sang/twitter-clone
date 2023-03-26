870c8f1349a0430400075ed7719eca5286f768fc
KHC <the2sang@gmail.com>
2023-03-27 00:20:55 +0900

    first commit

---
+++ components/layout/SidebarLogo.tsx
@@ -0,0 +1,27 @@
+import {BsTwitter} from "react-icons/bs";
+import {useRouter} from "next/router";
+
+const SidebarLogo = () => {
+  const router = useRouter();
+  return (
+    <div
+      onClick={() => router.push('/')}
+      className="
+        rounded-full
+        h-14
+        w-14
+        p-4
+        flex
+        items-center
+        justify-center
+        hover:bg-blue-30
+        hover:bg-opacity-10
+        cursor-pointer
+        transition
+    ">
+      <BsTwitter size={28} color="white"/>
+    </div>
+  );
+}
+
+export default SidebarLogo;
